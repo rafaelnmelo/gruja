c423ad2632f12ec8fb586c31e6ffa840ead9cc23
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-18 09:44:20 -0300

    Utilizando Page Object

--- Gruja/GrujaUITests/GrujaUITests.swift
+++ Gruja/GrujaUITests/GrujaUITests.swift
@@ -3,22 +3,22 @@ import XCTest
 class when_content_view_is_shown: XCTestCase {
     
     private var app: XCUIApplication!
+    private var contentViewPage: ContentViewPage!
     
     override func setUp() {
         super.setUp()
         app = XCUIApplication()
+        contentViewPage = ContentViewPage(app: app)
         continueAfterFailure = false
         app.launch()
     }
   
     func test_should_make_sure_that_the_total_text_field_contains_default_value() {
-        let totalTextField = app.textFields["totalTextField"]
-        XCTAssertEqual(totalTextField.value as! String, "Enter total")
+        XCTAssertEqual(contentViewPage.totalTextField.value as! String, "Enter total")
     }
     
     func test_should_make_sure_the_20_percent_default_tip_option_is_selected() {
-        let tipPercentageSegmentedControl = app.segmentedControls["tipPercentageSegmentedControl"]
-        let segmentedControlButton = tipPercentageSegmentedControl.buttons.element(boundBy: 1)
+        let segmentedControlButton = contentViewPage.tipPercentageSegmentedControl.buttons.element(boundBy: 1)
         XCTAssertEqual(segmentedControlButton.label, "20%")
         XCTAssertTrue(segmentedControlButton.isSelected)
     }
@@ -31,19 +31,19 @@ class when_content_view_is_shown: XCTestCase {
 
 class when_calculate_tip_button_is_pressed_for_valid_input: XCTestCase {
     private var app: XCUIApplication!
+    private var contentViewPage: ContentViewPage!
     
     override func setUp() {
         super.setUp()
         app = XCUIApplication()
+        contentViewPage = ContentViewPage(app: app)
         continueAfterFailure = false
         app.launch()
         
-        let totalTextField = app.textFields["totalTextField"]
-        totalTextField.tap()
-        totalTextField.typeText("100")
+        contentViewPage.totalTextField.tap()
+        contentViewPage.totalTextField.typeText("100")
         
-        let calculateTipButton = app.buttons["calculateTipButton"]
-        calculateTipButton.tap()
+        contentViewPage.calculateTipButton.tap()
     }
     
     func test_should_make_sure_that_tip_is_displayed_on_the_screen() {
@@ -55,31 +55,29 @@ class when_calculate_tip_button_is_pressed_for_valid_input: XCTestCase {
 
 class when_calculate_tip_button_is_pressed_for_invalid_input: XCTestCase {
     private var app: XCUIApplication!
+    private var contentViewPage: ContentViewPage!
     
     override func setUp() {
         super.setUp()
         app = XCUIApplication()
+        contentViewPage = ContentViewPage(app: app)
         continueAfterFailure = false
         app.launch()
         
-        let totalTextField = app.textFields["totalTextField"]
-        totalTextField.tap()
-        totalTextField.typeText("-100")
+        contentViewPage.totalTextField.tap()
+        contentViewPage.totalTextField.typeText("-100")
         
-        let calculateTipButton = app.buttons["calculateTipButton"]
-        calculateTipButton.tap()
+        contentViewPage.calculateTipButton.tap()
     }
     
     func test_should_clear_tip_label_for_invalid_input() {
-        let tipText = app.staticTexts["tipText"]
-        let _ = tipText.waitForExistence(timeout: 0.5)
-        XCTAssertEqual(tipText.label, "")
+        let _ = contentViewPage.tipText.waitForExistence(timeout: 0.5)
+        XCTAssertEqual(contentViewPage.tipText.label, "")
     }
     
     func test_should_display_error_message_for_invalid_input() {
-        let messageText = app.staticTexts["messageText"]
-        let _ = messageText.waitForExistence(timeout: 0.5)
-        XCTAssertEqual(messageText.label, "Invalid Input")
+        let _ = contentViewPage.messageText.waitForExistence(timeout: 0.5)
+        XCTAssertEqual(contentViewPage.messageText.label, "Invalid Input")
     }
 }
 
